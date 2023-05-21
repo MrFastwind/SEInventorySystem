18d78a7f5766fadfbc4a0909557622b5f646435b
MrFastwind <nivek1999@yahoo.it>
2023-05-21 18:24:07 +0200

    Removes Check for components Edits debug message on quering items Cleanup auto generated comments and methods

--- SEInventorySystem/Program.cs
+++ SEInventorySystem/Program.cs
@@ -9,31 +9,11 @@ using VRage.Game.ModAPI.Ingame;
 namespace IngameScript {
     partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
 
-        List<IMyCubeBlock> inventoriesBlocks = new List<IMyCubeBlock>();
-        AssemblerGroup assemblersGroup = new AssemblerGroup();
-        Dictionary<string, int> inventories = new Dictionary<string, int>();
-        Dictionary<string, int> wantedValues = new Dictionary<string, int> {
+        private List<IMyCubeBlock> inventoriesBlocks = new List<IMyCubeBlock>();
+        private AssemblerGroup assemblersGroup = new AssemblerGroup();
+        private Dictionary<string, int> inventories = new Dictionary<string, int>();
+        private Dictionary<string, int> wantedValues = new Dictionary<string, int> {
             {"BulletproofGlass", 0 },
             {"Canvas", 0 },
             {"Computer", 0 },
@@ -59,31 +39,11 @@ namespace IngameScript {
             {"ZoneChip", 0 }
         };
 
-        Dictionary<string, int> itemsStock = new Dictionary<string, int>();
-        Dictionary<string, int> itemsQueue = new Dictionary<string, int>();
+        private Dictionary<string, int> itemsStock = new Dictionary<string, int>();
+        private Dictionary<string, int> itemsQueue = new Dictionary<string, int>();
 
         public Program() {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-            // 
-            // It's recommended to set Runtime.UpdateFrequency 
-            // here, which will allow your script to run itself without a 
-            // timer block.
-
             Runtime.UpdateFrequency = UpdateFrequency.Update100;
-        }
-
-        public void Save() {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
         }
 
         public void Main(string argument, UpdateType updateSource) {
@@ -105,15 +65,13 @@ namespace IngameScript {
             {
                 if (item.Value > 0) {
                     Echo($"{item.Key}: w: {wantedValues[item.Key]}, s: {itemsStock.GetValueOrDefault(item.Key, -1)}, q: {itemsQueue.GetValueOrDefault(item.Key, -1)}");
-
                 }
-
             }
 
             foreach (var item in wantedValues){
                 var offset = item.Value - (itemsStock.GetValueOrDefault(item.Key, 0) + itemsQueue.GetValueOrDefault(item.Key, 0));
                 if (offset > 0) {
-                    Echo($"Quering {item.Key}");
+                    Echo($"Quering {item.Key}, with blueprint {LabelConverter.GetBlueprintDefinitionByName(item.Key)}");
                     AddItemToProductionQueue(item.Key, offset);
                 }
                                   
@@ -130,6 +88,8 @@ namespace IngameScript {
         }
 
         private static bool IsComponent(MyItemType type) => type.TypeId.Contains("Component");
+
+        private static bool IsMagazine(MyItemType type) => type.TypeId.Contains("AmmoMagazine");
 
         private bool IsTracked(MyItemType type) => wantedValues.ContainsKey(GetItemName(type));
 
@@ -162,7 +122,7 @@ namespace IngameScript {
                 }
                 //if (gridInventory == null || gridInventory.Count == 0) continue;
             }
-            IEnumerable<MyInventoryItem> stocks = gridInventory.Where(x => IsComponent(x.Type) && IsTracked(x.Type));
+            IEnumerable<MyInventoryItem> stocks = gridInventory.Where(x => IsTracked(x.Type));
             //Echo($"Filtered: {stocks.Count()}, InventorySlosts {gridInventory.Count}");
             foreach (var item in stocks) {
                 var value = 0;
@@ -184,7 +144,6 @@ namespace IngameScript {
                     itemsQueue.TryGetValue(name, out value);
                     itemsQueue[name] = value + (int)item.Amount;
                 }
-                
             }
         }
     }
